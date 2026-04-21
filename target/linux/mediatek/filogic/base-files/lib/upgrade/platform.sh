59cd8e70f242cc7e2f0fb26a1f4c71f7716a510e
BeeconMini <zhonghaodong@beeconx.com.cn>
2026-04-21 07:05:30 +0000

    适配BeeconMini SEED AC2

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -271,6 +271,7 @@ platform_do_upgrade() {
 		CI_ROOT_UBIPART=ubi
 		nand_do_upgrade "$1"
 		;;
+	beeconmini,seed-ac2|\
 	beeconmini,seed-ac1)
 		CI_KERNPART="kernel"
 		CI_ROOTPART="rootfs"
@@ -373,6 +374,7 @@ platform_copy_config() {
 	airpi,ap3000m|\
 	arcadyan,mozart|\
 	beeconmini,seed-ac1|\
+	beeconmini,seed-ac2|\
 	glinet,gl-mt2500|\
 	glinet,gl-mt2500-airoha|\
 	glinet,gl-mt6000|\
--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -824,6 +824,16 @@ define Device/beeconmini_seed-ac1
 endef
 TARGET_DEVICES += beeconmini_seed-ac1
 
+define Device/beeconmini_seed-ac2
+  DEVICE_VENDOR := BeeconMini
+  DEVICE_MODEL := SEED AC2
+  DEVICE_DTS := mt7981b-beeconmini-seed-ac2
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-fs-f2fs kmod-fs-ext4 mkf2fs e2fsprogs kmod-switch-rtl8373 mt7981-wo-firmware
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += beeconmini_seed-ac2
+
 define Device/buffalo_wsr-6000ax8
   DEVICE_MODEL := WSR-6000AX8
   DEVICE_VENDOR := Buffalo
