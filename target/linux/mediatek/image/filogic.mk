5975d97f93a923ff8ccd69906aac795a637d0910
BeeconMini <zhonghaodong@beeconx.com.cn>
2026-04-21 06:40:27 +0000

    适配BeeconMini SEED AC1

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -814,6 +814,16 @@ define Device/bazis_ax3000wm
 endef
 TARGET_DEVICES += bazis_ax3000wm
 
+define Device/beeconmini_seed-ac1
+  DEVICE_VENDOR := BeeconMini
+  DEVICE_MODEL := SEED AC1
+  DEVICE_DTS := mt7981b-beeconmini-seed-ac1
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-fs-f2fs kmod-fs-ext4 mkf2fs e2fsprogs kmod-usb3 kmod-sfp kmod-xs2184 mt7981-wo-firmware
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += beeconmini_seed-ac1
+
 define Device/buffalo_wsr-6000ax8
   DEVICE_MODEL := WSR-6000AX8
   DEVICE_VENDOR := Buffalo
